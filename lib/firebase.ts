663dfaf667ba50a917879d2d8e386dbb9add28b9
comfreec1 <comfreec1@gmail.com>
2026-03-25 01:36:18 +0900

    refactor: move firebase config to env variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "REMOVED_FROM_HISTORY",
-  authDomain: "location-87f14.firebaseapp.com",
-  databaseURL: "https://location-87f14-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "location-87f14",
-  storageBucket: "location-87f14.firebasestorage.app",
-  messagingSenderId: "100803979998",
-  appId: "1:100803979998:web:d98868cf32913d3f4a304f",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
